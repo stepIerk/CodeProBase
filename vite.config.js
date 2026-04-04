5cbe87d7ad73fc4acaaf843a79d014d1d9de8377
Stepan2 <stepan2@df14.local>
2026-04-04 12:55:39 +0300

    gh-pages path update 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CodeProBase/src',  // ← заменить на имя твоего GitHub репозитория
+  base: '/CodeProBase/src/',  // ← заменить на имя твоего GitHub репозитория
 })
